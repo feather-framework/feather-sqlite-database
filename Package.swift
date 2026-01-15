916cb992be053c4cdbdd9c4cef3fe23a40dbe1a8
Tibor Bodecs <mail.tib@gmail.com>
2026-01-15 20:21:33 +0100

    swift 6 updates

--- Package.swift
+++ Package.swift
@@ -1,36 +1,60 @@
-// swift-tools-version:5.9
+// swift-tools-version:6.1
 import PackageDescription
 
+// NOTE: https://github.com/swift-server/swift-http-server/blob/main/Package.swift
+var defaultSwiftSettings: [SwiftSetting] =
+[
+    // https://github.com/swiftlang/swift-evolution/blob/main/proposals/0441-formalize-language-mode-terminology.md
+    .swiftLanguageMode(.v6),
+    // https://github.com/swiftlang/swift-evolution/blob/main/proposals/0444-member-import-visibility.md
+    .enableUpcomingFeature("MemberImportVisibility"),
+    // https://forums.swift.org/t/experimental-support-for-lifetime-dependencies-in-swift-6-2-and-beyond/78638
+    .enableExperimentalFeature("Lifetimes"),
+    // https://github.com/swiftlang/swift/pull/65218
+    .enableExperimentalFeature("AvailabilityMacro=featherSQLiteDatabase 1.0:macOS 15.0, iOS 18.0, watchOS 11.0, tvOS 18.0, visionOS 2.0"),
+]
+
+#if compiler(>=6.2)
+defaultSwiftSettings.append(
+    // https://github.com/swiftlang/swift-evolution/blob/main/proposals/0461-async-function-isolation.md
+    .enableUpcomingFeature("NonisolatedNonsendingByDefault")
+)
+#endif
+
+
 let package = Package(
-    name: "feather-database-driver-sqlite",
+    name: "feather-sqlite-database",
     platforms: [
-        .macOS(.v13),
-        .iOS(.v16),
-        .tvOS(.v16),
-        .watchOS(.v9),
-        .visionOS(.v1),
+        .macOS(.v15),
+        .iOS(.v18),
+        .tvOS(.v18),
+        .watchOS(.v11),
+        .visionOS(.v2),
     ],
     products: [
-        .library(name: "FeatherDatabaseDriverSQLite", targets: ["FeatherDatabaseDriverSQLite"]),
+        .library(name: "FeatherSQLiteDatabase", targets: ["FeatherSQLiteDatabase"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/feather-framework/feather-database", .upToNextMinor(from: "0.4.0")),
-        .package(url: "https://github.com/vapor/sqlite-kit", from: "4.0.0"),
+        .package(url: "https://github.com/apple/swift-log", from: "1.6.0"),
+        .package(url: "https://github.com/vapor/sqlite-nio", from: "1.12.0"),
+        .package(url: "https://github.com/feather-framework/feather-database", branch: "feature/swift-6"),
     ],
     targets: [
         .target(
-            name: "FeatherDatabaseDriverSQLite",
+            name: "FeatherSQLiteDatabase",
             dependencies: [
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "SQLiteNIO", package: "sqlite-nio"),
                 .product(name: "FeatherDatabase", package: "feather-database"),
-                .product(name: "SQLiteKit", package: "sqlite-kit"),
-            ]
+            ],
+            swiftSettings: defaultSwiftSettings
         ),
         .testTarget(
-            name: "FeatherDatabaseDriverSQLiteTests",
+            name: "FeatherSQLiteDatabaseTests",
             dependencies: [
-                .product(name: "FeatherDatabaseTesting", package: "feather-database"),
-                .target(name: "FeatherDatabaseDriverSQLite"),
-            ]
+                .target(name: "FeatherSQLiteDatabase"),
+            ],
+            swiftSettings: defaultSwiftSettings
         ),
     ]
 )
