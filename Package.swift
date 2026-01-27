ebf399514ed728f0703dd7218ac719ac71541f00
Tibor Bodecs <mail.tib@gmail.com>
2026-01-27 09:19:05 +0100

    fix docc

--- Package.swift
+++ Package.swift
@@ -38,6 +38,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-log", from: "1.6.0"),
         .package(url: "https://github.com/vapor/sqlite-nio", from: "1.12.0"),
         .package(url: "https://github.com/feather-framework/feather-database", exact: "1.0.0-beta.2"),
+        // [docc-plugin-placeholder]
     ],
     targets: [
         .target(
